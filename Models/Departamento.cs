b79b2284d996ea9c1d6ff86934fc2a00691adc75
KevinArellanoMtz <55411633+KevinArellanoMtz@users.noreply.github.com>
2023-10-14 21:00:54 -0600

    Models

---
+++ Models/Departamento.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace RPInventarios.Models;
+
+public class Departamento
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    [Required(ErrorMessage = "El nombre es requerido")]
+    [MinLength(4,ErrorMessage ="Minimo 4 caracteres")]
+    [MaxLength(100,ErrorMessage ="Maximo 100 caracteres")]
+    [Display(Name = "Departamento")]
+
+    public string Descripcion { get; set; }
+    [MinLength(5,ErrorMessage ="Minimo 5 caracteres")]
+    [MaxLength(200,ErrorMessage ="Maximo 200 caracteres")]
+
+    //public List<DateTime> FechaCreacion { get; set; }
+
+
+    
+    public virtual ICollection<Producto> Productos { get; set; }
+}
